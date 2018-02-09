2758f26d1a77c59767629fc8769ba4870c9a654f
Konstantin Lazukin <konstantin.lazukin@gmail.com>
2018-02-09 06:31:07 +0200

    Add CallAndRethrowM macro to scip writing method name

--- include/EverydayTools/Exception/CallAndRethrow.h
+++ include/EverydayTools/Exception/CallAndRethrow.h
@@ -10,3 +10,26 @@ decltype(auto) CallAndRethrow(const TChar* rethrowMessage, F&& f) {
 		std::throw_with_nested(Exception(rethrowMessage));
 	}
 }
+
+namespace call_and_rethrow_helpers {
+    struct Caller {
+        Caller(const char* name) :
+            m_name(name)
+        {
+        }
+
+        const char* m_name = nullptr;
+    
+        template<typename Functor>
+        decltype(auto) operator+(Functor&& functor) const {
+            return CallAndRethrow(m_name, std::move(functor));
+        }
+    };
+}
+
+#ifdef _MSC_VER
+#define CallAndRethrowM call_and_rethrow_helpers::Caller(__FUNCTION__)
+#elif
+// Need some warning here?
+#define CallAndRethrowM call_and_rethrow_helpers::Caller(__func__)
+#endif
