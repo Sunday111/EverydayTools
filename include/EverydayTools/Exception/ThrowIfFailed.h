e5be8c5bd78037e60af6243e7ad575bbe98f699d
Konstantin Lazukin <konstantin.lazukin@gmail.com>
2018-03-17 20:04:09 +0200

    Add possibility to specify character type in ThrowIfFailed function

--- include/EverydayTools/Exception/ThrowIfFailed.h
+++ include/EverydayTools/Exception/ThrowIfFailed.h
@@ -5,10 +5,10 @@
 
 namespace edt
 {
-    template<typename Exception = std::runtime_error, typename... Args>
+    template<typename Exception = std::runtime_error, typename TChar = char, typename... Args>
     void ThrowIfFailed(bool condition, Args&&... args) {
         if (!condition) {
-            std::stringstream stream;
+            std::basic_stringstream<TChar> stream;
             std::initializer_list<int> {
                 (stream << std::forward<Args>(args), 0)...
             };
