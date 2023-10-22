588dbe80b17f556c83a0af18e12d36316bfd4c98
Konstantin Lazukin <konstantin.lazukin@gmail.com>
2023-10-22 18:11:47 +0300

    Fix for newest clang version

--- include/EverydayTools/Geom/Details/Mixins/Common.hpp
+++ include/EverydayTools/Geom/Details/Mixins/Common.hpp
@@ -1,100 +1,119 @@
 #pragma once
 
+#include <cstddef>
 #include <type_traits>
 
 #include "../ImplementCastThis.hpp"
 
-namespace edt::geom::details::common {
-template <typename T, size_t nRows, size_t nColumns,
-          template <typename, size_t, size_t> typename Final,
-          template <typename, size_t, size_t> typename ReturnValue>
-class Mixin {
-  EDT_MATRIX_IMPLEMENT_CAST_THIS
- public:
-  template <typename U, template <typename, size_t, size_t> typename Mtx>
-  TFinal& Add(const Mtx<U, nRows, nColumns>& that_) noexcept {
-    auto& this_ = CastThis();
-    for (size_t i = 0; i < nRows; ++i) {
-      for (size_t j = 0; j < nColumns; ++j) {
-        this_.At(i, j) += that_.At(i, j);
-      }
+namespace edt::geom::details::common
+{
+template <
+    typename T,
+    size_t nRows,
+    size_t nColumns,
+    template <typename, size_t, size_t>
+    typename Final,
+    template <typename, size_t, size_t>
+    typename ReturnValue>
+class Mixin
+{
+    EDT_MATRIX_IMPLEMENT_CAST_THIS
+public:
+    template <typename U, template <typename, size_t, size_t> typename Mtx>
+    TFinal& Add(const Mtx<U, nRows, nColumns>& that_) noexcept
+    {
+        auto& this_ = CastThis();
+        for (size_t i = 0; i < nRows; ++i)
+        {
+            for (size_t j = 0; j < nColumns; ++j)
+            {
+                this_.At(i, j) += that_.At(i, j);
+            }
+        }
+        return this_;
     }
-    return this_;
-  }
 
-  TFinal& Add(const T& value) noexcept {
-    auto& this_ = CastThis();
-    for (size_t i = 0; i < nRows; ++i) {
-      for (size_t j = 0; j < nColumns; ++j) {
-        this_.At(i, j) += value;
-      }
+    TFinal& Add(const T& value) noexcept
+    {
+        auto& this_ = CastThis();
+        for (size_t i = 0; i < nRows; ++i)
+        {
+            for (size_t j = 0; j < nColumns; ++j)
+            {
+                this_.At(i, j) += value;
+            }
+        }
+        return this_;
     }
-    return this_;
-  }
 
-  template <typename U>
-  TFinal& Multiply(U value) noexcept {
-    auto& this_ = CastThis();
-    for (size_t i = 0; i < nRows; ++i) {
-      for (size_t j = 0; j < nColumns; ++j) {
-        this_.At(i, j) *= value;
-      }
+    template <typename U>
+    TFinal& Multiply(U value) noexcept
+    {
+        auto& this_ = CastThis();
+        for (size_t i = 0; i < nRows; ++i)
+        {
+            for (size_t j = 0; j < nColumns; ++j)
+            {
+                this_.At(i, j) *= value;
+            }
+        }
+        return this_;
     }
-    return this_;
-  }
 
-  TFinal& Fill(const T& value) noexcept {
-    auto& this_ = CastThis();
-    for (size_t i = 0; i < nRows; ++i) {
-      for (size_t j = 0; j < nColumns; ++j) {
-        this_.At(i, j) = value;
-      }
+    TFinal& Fill(const T& value) noexcept
+    {
+        auto& this_ = CastThis();
+        for (size_t i = 0; i < nRows; ++i)
+        {
+            for (size_t j = 0; j < nColumns; ++j)
+            {
+                this_.At(i, j) = value;
+            }
+        }
+        return this_;
     }
-    return this_;
-  }
 
-  template <typename U>
-  TFinal& operator*=(U value) noexcept {
-    return Multiply(value);
-  }
-
-  template <template <typename, size_t, size_t> typename Mtx>
-  void Copy(const Mtx<T, nRows, nColumns>& that_) noexcept {
-    auto& this_ = CastThis();
-    for (size_t r = 0; r < nRows; ++r) {
-      for (size_t c = 0; c < nColumns; ++c) {
-        this_.At(r, c) = that_.At(r, c);
-      }
+    template <typename U>
+    TFinal& operator*=(U value) noexcept
+    {
+        return Multiply(value);
     }
-  }
 
-  template <typename U>
-  constexpr ReturnValue<T, nRows, nColumns> operator*(U value) const noexcept {
-    ReturnValue<T, nRows, nColumns> result;
-    result.Copy(CastThis());
-    // auto result = static_cast<ReturnValue<T, nRows, nColumns>>(CastThis());
-    // ReturnValue<T, nRows, nColumns> result(CastThis());
-    result *= value;
-    return result;
-  }
+    template <template <typename, size_t, size_t> typename Mtx>
+    void Copy(const Mtx<T, nRows, nColumns>& that_) noexcept
+    {
+        auto& this_ = CastThis();
+        for (size_t r = 0; r < nRows; ++r)
+        {
+            for (size_t c = 0; c < nColumns; ++c)
+            {
+                this_.At(r, c) = that_.At(r, c);
+            }
+        }
+    }
 
-  template <typename U, template <typename, size_t, size_t> typename Mtx>
-  TFinal& operator+=(const Mtx<U, nRows, nColumns>& that_) noexcept {
-    return Add(that_);
-  }
+    template <typename U, template <typename, size_t, size_t> typename Mtx>
+    TFinal& operator+=(const Mtx<U, nRows, nColumns>& that_) noexcept
+    {
+        return Add(that_);
+    }
 
-  template <typename U, template <typename, size_t, size_t> typename Mtx>
-  ReturnValue<T, nRows, nColumns> operator+(
-      const Mtx<U, nRows, nColumns>& that_) const noexcept {
-    ReturnValue<T, nRows, nColumns> result(CastThis());
-    result += that_;
-    return result;
-  }
+    template <typename U, template <typename, size_t, size_t> typename Mtx>
+    ReturnValue<T, nRows, nColumns> operator+(const Mtx<U, nRows, nColumns>& that_) const noexcept
+    {
+        ReturnValue<T, nRows, nColumns> result(CastThis());
+        result += that_;
+        return result;
+    }
 
-  [[nodiscard]] constexpr static size_t RowsCount() noexcept { return nRows; }
+    [[nodiscard]] constexpr static size_t RowsCount() noexcept
+    {
+        return nRows;
+    }
 
-  [[nodiscard]] constexpr static size_t ColumnsCount() noexcept {
-    return nColumns;
-  }
+    [[nodiscard]] constexpr static size_t ColumnsCount() noexcept
+    {
+        return nColumns;
+    }
 };
 }  // namespace edt::geom::details::common
--- include/EverydayTools/Geom/Details/Mixins/MatrixMultiplication.hpp
+++ include/EverydayTools/Geom/Details/Mixins/MatrixMultiplication.hpp
@@ -1,43 +1,66 @@
 #pragma once
 
+#include <cstddef>
+#include <type_traits>
+
 #include "EverydayTools/Geom/Details/ImplementCastThis.hpp"
 
-namespace edt::geom::details::matrix_multiplication {
-template <typename T, size_t nRows, size_t nColumns,
-          template <typename, size_t, size_t> typename Final,
-          template <typename, size_t, size_t> typename ReturnValue>
-class Mixin {
-  EDT_MATRIX_IMPLEMENT_CAST_THIS
- public:
-  template <typename U, size_t bColumns,
-            template <typename, size_t, size_t> typename Mtx>
-  decltype(auto) Multiply(const Mtx<U, nColumns, bColumns>& that_) const {
-    // nRows is n
-    // nColumns is m
-    // bColumns is p
+namespace edt::geom::details::matrix_multiplication
+{
+template <
+    typename T,
+    size_t nRows,
+    size_t nColumns,
+    template <typename, size_t, size_t>
+    typename Final,
+    template <typename, size_t, size_t>
+    typename ReturnValue>
+class Mixin
+{
+    EDT_MATRIX_IMPLEMENT_CAST_THIS
+public:
+    template <typename U, size_t bColumns, template <typename, size_t, size_t> typename Mtx>
+    decltype(auto) Multiply(const Mtx<U, nColumns, bColumns>& that_) const
+    {
+        // nRows is n
+        // nColumns is m
+        // bColumns is p
 
-    // using TR = decltype(std::declval(T) * std::declval(U));
-    using TR = T;
-    ReturnValue<TR, nRows, bColumns> result;
+        // using TR = decltype(std::declval(T) * std::declval(U));
+        using TR = T;
+        ReturnValue<TR, nRows, bColumns> result;
 
-    auto& this_ = CastThis();
+        auto& this_ = CastThis();
 
-    for (size_t i = 0; i < nRows; ++i) {
-      for (size_t j = 0; j < bColumns; ++j) {
-        auto this_row = this_.GetRow(i);
-        auto that_column = that_.GetColumn(j);
-        auto dot_prod = this_row.Dot(that_column);
-        result.At(i, j) = dot_prod;
-      }
+        for (size_t i = 0; i < nRows; ++i)
+        {
+            for (size_t j = 0; j < bColumns; ++j)
+            {
+                auto this_row = this_.GetRow(i);
+                auto that_column = that_.GetColumn(j);
+                auto dot_prod = this_row.Dot(that_column);
+                result.At(i, j) = dot_prod;
+            }
+        }
+
+        return result;
     }
 
-    return result;
-  }
+    template <typename U, size_t bColumns, template <typename, size_t, size_t> typename Mtx>
+    decltype(auto) operator*(const Mtx<U, nColumns, bColumns>& that_) const
+    {
+        return Multiply(that_);
+    }
 
-  template <typename U, size_t bColumns,
-            template <typename, size_t, size_t> typename Mtx>
-  decltype(auto) operator*(const Mtx<U, nColumns, bColumns>& that_) const {
-    return Multiply(that_);
-  }
+    template <typename U, typename Enable = std::enable_if_t<(std::is_floating_point_v<U> || std::is_integral_v<U>)>>
+    constexpr ReturnValue<T, nRows, nColumns> operator*(U value) const noexcept
+    {
+        ReturnValue<T, nRows, nColumns> result;
+        result.Copy(CastThis());
+        // auto result = static_cast<ReturnValue<T, nRows, nColumns>>(CastThis());
+        // ReturnValue<T, nRows, nColumns> result(CastThis());
+        result *= value;
+        return result;
+    }
 };
 }  // namespace edt::geom::details::matrix_multiplication
