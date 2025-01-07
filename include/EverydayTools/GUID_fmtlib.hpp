6d8bcdf0bf240c5be9e0a01bb66f4059e3e793a7
Kostiantyn Lazukin <konstantin.lazukin@gmail.com>
2025-01-07 23:25:15 +0000

    Fix formatter for guid

--- include/EverydayTools/GUID_fmtlib.hpp
+++ include/EverydayTools/GUID_fmtlib.hpp
@@ -7,7 +7,7 @@ template <>
 struct fmt::formatter<edt::GUID> : fmt::formatter<std::string>
 {
     template <typename FormatContext>
-    auto format(const edt::GUID& guid, FormatContext& ctx)
+    static constexpr auto format(const edt::GUID& guid, FormatContext& ctx)
     {
         const auto data = guid.ToCharArray();
         return fmt::format_to(ctx.out(), "{}", std::string_view(data.data(), data.size()));
